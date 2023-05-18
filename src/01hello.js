4c7daf5fe54ead0ed1916cffe4d61e0c537e3aab
LijiaHou <ljhou@foxmail.com>
2023-05-18 16:48:14 +0800

    run it

---
+++ src/01hello.js
@@ -0,0 +1,11 @@
+// import Koa from 'koa';
+
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(ctx => {
+  ctx.body = 'Hello Koa';
+});
+
+app.listen(3000);
--- src/hello.js
+++
@@ -1,8 +0,0 @@
-const Koa = requier('koa');
-const app = new Koa();
-
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
-
-app.listen(3000);
